b9c93e87300c952596385fdd1b51a2ea6fb73d44
hausman-gdit <steven.hausman@gdit.com>
2021-02-04 12:17:46 -0500

    expand error docstring

--- chemreg/indigo/errors.py
+++ chemreg/indigo/errors.py
@@ -1,13 +1,21 @@
 import functools
+from typing import Any
 
 from indigo import IndigoException
 
 
-def catch_compound_calc_error(f):
+def catch_compound_calc_error(f: Any) -> Any:
     """
     A function wrapper for catching calculation errors.
     Primarially for catching 'indigo.IndigoException: element: bad valence...'
     errors and ensuring a 200 response.
+
+    Args:
+        f: a function to be wrapped.
+
+    Returns:
+        Either a calculated value (MW, MF, SMILES, Inchikey) or `null`
+
     """
 
     @functools.wraps(f)
